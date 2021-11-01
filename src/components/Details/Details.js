94b47d430fbd6feb86534e033f0e7db7dbe5f514
abrar-hossain <abrarmashuk@gmail.com>
2021-11-01 20:00:27 +0600

    first commit

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -8,7 +8,7 @@ const Details = () => {
     const [service, setService] = useState([]);
     const { serviceId } = useParams();
     useEffect(() => {
-        fetch(`http://localhost:5000/services/${serviceId}`)
+        fetch(`https://guarded-eyrie-73538.herokuapp.com/services/${serviceId}`)
             .then(res => res.json())
             .then(data => setService(data));
     }, [])
--- src/hooks/useServices.js
+++ src/hooks/useServices.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const useServices = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch("http://localhost:5000/services")
+        fetch("https://guarded-eyrie-73538.herokuapp.com/services")
             .then((res) => res.json())
             .then((data) => setServices(data));
     }, []);
